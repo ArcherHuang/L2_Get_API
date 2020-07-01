415267dc132501ec52148020789b41ab7f565f95
ArcherHuang <mmosconii@gmail.com>
2020-07-01 21:52:09 +0800

    Add Cart Btn

--- action.js
+++ action.js
@@ -45,7 +45,9 @@ const obj = {
           <h5 class="card-title">${item.title}</h5>
           <p class="card-text">${item.content}</p>
           <p class="card-text">${item.description}</p>
-          <span class="badge badge-primary mr-3">特價: ${item.price} </span><span class="original-price">原價: ${item.origin_price}</span>
+          <span class="badge badge-primary mr-3">特價: ${item.price}</span>
+          <span class="original-price">原價: ${item.origin_price}</span>
+          <a href="#" class="btn btn-outline-success mt-4 add-cart" style="display: block;margin:10px auto;">加入購物車</a>
         </div>
       </div>`;
     });
