d56e5b4a25a0df499cf90439da2d0656ec079c5d
ArcherHuang <mmosconii@gmail.com>
2020-07-01 20:34:19 +0800

    Update View

--- action.js
+++ action.js
@@ -1,11 +1,13 @@
-var obj = {
+const obj = {
   data: {
-    uuid: 'd191d308-9c29-486a-ab7d-35afd78c1b41',
+    profile: {
+      uuid: 'd191d308-9c29-486a-ab7d-35afd78c1b41',
+    },
     products: [],
   },
-  getData: function () {
-    var vm = this;
-    var url = `https://course-ec-api.hexschool.io/api/${this.data.uuid}/ec/products`;
+  getProducts() {
+    const vm = this;
+    const url = `https://course-ec-api.hexschool.io/api/${this.data.profile.uuid}/ec/products`;
 
     axios.get(url)
       .then(function (response) {
@@ -13,22 +15,23 @@ var obj = {
         vm.render();
       })
   },
-  render: function () {
-    var app = document.getElementById('app');
-    var products = this.data.products;
-    var str = '';
-    products.forEach(function (item) {
+  render() {
+    const app = document.getElementById('app');
+    const products = this.data.products;
+    let str = '';
+    products.forEach(item => {
       str += `
-<div class="card">
-<img src="${ item.imageUrl[0]}" class="card-img-top">
-<div class="card-body">
-<h5 class="card-title">${ item.title}</h5>
-<p class="card-text">${ item.content}</p>
-</div>
-</div>`;
+      <div class="card">
+        <img src="${item.imageUrl[0]}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <p class="card-text">${item.content}</p>
+          <span class="badge badge-primary">特價: ${item.price}</span>
+        </div>
+      </div>`;
     });
     app.innerHTML = str;
   }
 }
 
-obj.getData();
+obj.getProducts();
